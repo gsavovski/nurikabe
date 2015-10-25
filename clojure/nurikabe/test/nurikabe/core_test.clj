46049890d9e3727a091cfb78276c59e3af13ef52
Goran Savovski <gsavovski@gmail.com>
2015-10-25 10:03:49 -0500

    first failing midje fact

--- clojure/nurikabe/test/nurikabe/core_test.clj
+++ clojure/nurikabe/test/nurikabe/core_test.clj
@@ -1,7 +1,13 @@
-(ns nurikabe.core-test
-  (:require [clojure.test :refer :all]
-            [nurikabe.core :refer :all]))
+; (ns nurikabe.core-test
+;   (:require [clojure.test :refer :all]
+;             [nurikabe.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(ns nurikabe.core-test
+    (:use midje.sweet))
+    ; (:require nurikabe.core :refer :all))
+
+
+(fact  (+ 2 2) => 5)
+; (deftest a-test
+;   (testing "FIXME, I fail."
+;     (is (= 0 1))))
