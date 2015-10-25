fc4e61e186ac828b127a1a808b4fbd2ef2d780ce
Goran Savovski <gsavovski@gmail.com>
2015-10-25 09:56:21 -0500

    midje in da haus + initial terminal coloring output

--- clojure/nurikabe/project.clj
+++ clojure/nurikabe/project.clj
@@ -7,4 +7,7 @@
   :dependencies [[org.clojure/clojure "1.7.0"]]
   :main ^:skip-aot nurikabe.core
   :target-path "target/%s"
-  :profiles {:uberjar {:aot :all}})
+  :profiles  {{ :uberjar {:aot :all}}
+              { :dev  {:dependencies  [[midje  "1.6.3"]]
+                          :plugins  [[lein-midje  "3.1.3"]]}}}
+  )
